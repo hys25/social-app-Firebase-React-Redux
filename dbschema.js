21eb053cb26da0425fa4e50145becd663b77989b
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2021-04-29 12:23:11 +0300

    express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2021-04-29T09:18:30.513Z',
+      likeCount: 5,
+      commentsCount: 2,
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -3,39 +3,40 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello Galinie!");
-});
+const express = require('express');
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
-        console.log(doc.data())
+          screams.push({
+            screamId: doc.id,
+            body: doc.data().body,
+            userHandle: doc.data().userHandle,
+            createdAt: doc.data().createdAt
+          });
         }
       )
       return res.json(screams);
     })
     .catch((err) => console.error(err))
-});
+})
 
-exports.createScream= functions.https.onRequest((req, res) => {
-  if(req.method !== 'POST'){
-    return res.status(400).json({ error: 'Method not allowed'})
-  }
+app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
     user: req.body.userHandle,
-    createAt: admin.firestore.Timestamp.fromDate(new Date())
+    createAt: new Date().toISOString()
   }
 
-  admin.firestore()
+  admin
+    .firestore()
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -46,3 +47,5 @@ exports.createScream= functions.https.onRequest((req, res) => {
       console.error(err);
     })
 });
+
+exports.api = functions.https.onRequest(app);
